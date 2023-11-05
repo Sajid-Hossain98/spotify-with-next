35c309723fdbf15916ebefac1c023ec0581807a1
Sajid Hossain <bmsajidhossain420@gmail.com>
2023-11-05 19:03:38 +0600

    initial header for mobile

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,6 +3,9 @@
 import { useRouter } from "next/navigation";
 import { twMerge } from "tailwind-merge";
 import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
+import { HiHome } from "react-icons/Hi";
+import { BiSearch } from "react-icons/bi";
+import Button from "./Button";
 
 interface HeaderProps {
   children: React.ReactNode;
@@ -37,8 +40,25 @@ const Header: React.FC<HeaderProps> = ({ children, className }) => {
             <RxCaretRight size={35} className="text-white" />
           </button>
         </div>
+
+        <div className="flex md:hidden gap-x-2 items-center">
+          <button className="rounded-full p-2 bg-white flex items-center justify-center hover:opacity-75 transition">
+            <HiHome className="text-black" size={20} />
+          </button>
+          <button className="rounded-full p-2 bg-white flex items-center justify-center hover:opacity-75 transition">
+            <BiSearch className="text-black" size={20} />
+          </button>
+        </div>
+
+        <div className="flex justify-between items-center gap-x-4">
+          <>
+            <div>
+              <Button>Sign up</Button>
+            </div>
+          </>
+        </div>
       </div>
-      {children}
+      {/* {children} */}
     </div>
   );
 };
